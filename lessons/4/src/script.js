f86bc55e7d4f940044ae02840a5768eef3b08314
RealAdon <elia.ryser@gmail.com>
2025-07-29 16:27:40 +0200

    Final lesson 4

---
+++ lessons/4/src/script.js
@@ -0,0 +1,84 @@
+import * as THREE from 'three'
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * Objects
+ */
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+// Position
+mesh.position.set(0.7, -0.6, 1)
+
+// Scale
+mesh.scale.set(2, 0.5, 0.5)
+
+// Rotation
+// Reorder rotation to apply in the order of Y, X, Z
+mesh.rotation.reorder('YXZ')
+mesh.rotation.set(Math.PI / 4, Math.PI / 4, 0)
+
+ // Create a group
+const group = new THREE.Group()
+scene.add(group)
+
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+)
+cube1.position.x = -2
+group.add(cube1)
+
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0x0000ff })
+)
+cube2.position.x = 2
+group.add(cube2)
+
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1),
+    new THREE.MeshBasicMaterial({ color: 0xffff00 })
+)
+group.add(cube3)
+
+group.position.y = 2
+
+// Axes Helper
+const axesHelper = new THREE.AxesHelper(2)
+scene.add(axesHelper)
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+/**
+ * Camera
+ */
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
+scene.add(camera)
+
+camera.lookAt(axesHelper.position)
+
+console.log(mesh.position.distanceTo(camera.position))
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
